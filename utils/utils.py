ff802bd8358b5f77124c7bf9718ab5a399dd8858
Artymka <doomparser@gmail.com>
2025-02-14 18:22:24 +0300

    Add ignoring of distat lessons

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,7 @@ import icalendar
 from functools import cmp_to_key
 
 from utils.lesson import Lesson
-
+# from lesson import Lesson
 
 async def get_search_json(search_param: str) -> dict:
     url = "https://schedule-of.mirea.ru/schedule/api/search"
@@ -20,18 +20,6 @@ async def get_calendar(search_res: dict):
             res = await response.read()
             return icalendar.Calendar.from_ical(res)
 
-
-async def main():
-    search_res = await get_search_json("ИКБО-70-24")
-    cal = await get_calendar(search_res)
-
-    for event in cal.walk("VEVENT"):
-        print(event.get("SUMMARY"))
-        print(event.get("RRULE"))
-        # print(event)
-        print(event.get("DTEND").dt - event.get("DTSTART").dt)
-        # print(event.get("DTSTART").dt)
-
 async def get_lessons_sequence(cal: icalendar.Calendar) -> list[Lesson]:
     # search_res = await get_search_json("ИКБО-70-24")
     # cal = await get_calendar(search_res)
@@ -40,6 +28,8 @@ async def get_lessons_sequence(cal: icalendar.Calendar) -> list[Lesson]:
     for event in cal.walk("VEVENT"):
         # проверка на то, является ли event занятием
         if not event.get("RRULE"): continue
+        # проверка на то, является ли пара дистанционной
+        if event.get("LOCATION") == "Дистанционно (СДО)": continue
 
         lessons_sequence.append(Lesson(
             event.get("SUMMARY"),
@@ -57,6 +47,15 @@ async def get_lessons_sequence(cal: icalendar.Calendar) -> list[Lesson]:
 
     lessons_sequence.sort(key=cmp_to_key(compare))
     return lessons_sequence
+
+
+async def main():
+    search_res = await get_search_json("ИКБО-70-24")
+    cal = await get_calendar(search_res)
+    lessons = await get_lessons_sequence(cal)
+    for lesson in lessons:
+        print(lesson.room)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
